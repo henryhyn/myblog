c3b6c48a9e37fe8ec476baa521f9a1b6b260e073
何永能 <yongneng.he@dianping.com>
2018-06-26 16:47:21 +0800

    cors

--- api/views.py
+++ api/views.py
@@ -6,7 +6,17 @@ from myblog.settings import BASE_DIR
 model = gensim.models.KeyedVectors.load_word2vec_format(BASE_DIR + '/api/StaticData/out2', binary=False)
 
 
+def __build_response(rs):
+    response = HttpResponse(json.dumps(rs))
+    response['Access-Control-Allow-Origin'] = '*'
+    response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    response['Access-Control-Max-Age'] = '1000'
+    response['Access-Control-Allow-Headers'] = '*'
+    return response
+
+
 def similar_word(request):
     word = request.GET.get('word', '一般')
     topn = request.GET.get('topn', '10')
-    return HttpResponse(json.dumps(model.most_similar(word, topn=int(topn))))
+    rs = model.most_similar(word, topn=int(topn))
+    return __build_response(rs)
